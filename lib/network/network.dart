3b52bb875b659057bbfd1d6c50b8f737ca59e3f0
awalsetya <awalsetyanto.1993@gmail.com>
2020-05-30 21:26:23 +0700

    update registrasi.dart

---
+++ lib/network/network.dart
@@ -0,0 +1,7 @@
+class NetworkURL {
+  static String server = "http://localhost/ig/";
+
+  static String registrasi() {
+    return "$server/api/registrasi.php";
+  }
+}
--- lib/screen/registrasi.dart
+++ lib/screen/registrasi.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/custom/billaBongText.dart';
 import 'package:instagram_clone/custom/cutsomButton.dart';
+import 'package:http/http.dart' as http;
+import 'package:instagram_clone/network/network.dart';
 
 class Registrasi extends StatefulWidget {
   @override
@@ -9,6 +13,47 @@ class Registrasi extends StatefulWidget {
 
 class _RegistrasiState extends State<Registrasi> {
   var obseCure = true;
+  final _key = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  cek() {
+    if (_key.currentState.validate()) {
+      _key.currentState.save();
+      submit();
+    }
+  }
+
+  submit() async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Processing"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 16,
+                ),
+                Text("Please wait..."),
+              ],
+            ),
+          );
+        });
+    final response = await http.post(NetworkURL.registrasi(), body: {
+      "email": emailController.text.trim(),
+      "username": usernameController.text.trim(),
+      "password": passwordController.text.trim(),
+      "name": nameController.text.trim(),
+    });
+    final data = jsonDecode(response.body);
+    print(data);
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,80 +62,97 @@ class _RegistrasiState extends State<Registrasi> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
-              child: ListView(
-                padding: EdgeInsets.all(10),
-                children: <Widget>[
-                  BillabongText(
-                    "Instagram",
-                    fontsize: 50,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide(color: Colors.grey, width: 1),
-                      ),
+              child: Form(
+                key: _key,
+                child: ListView(
+                  padding: EdgeInsets.all(10),
+                  children: <Widget>[
+                    BillabongText(
+                      "Instagram",
+                      fontsize: 50,
                     ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Full Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide(color: Colors.grey, width: 1),
-                      ),
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Username",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide(color: Colors.grey, width: 1),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    obscureText: obseCure,
-                    decoration: InputDecoration(
-                        hintText: "Password",
+                    TextFormField(
+                      validator: (e) {
+                        if (e.isEmpty)
+                          return "Please input email address";
+                        else
+                          return null;
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(16),
                           borderSide: BorderSide(color: Colors.grey, width: 1),
                         ),
-                        suffixIcon: IconButton(
-                            icon: Icon(obseCure
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                            onPressed: () {
-                              setState(() {
-                                obseCure = !obseCure;
-                              });
-                            })),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  InkWell(
-                    child: CustomButton(
-                      "Sign In",
-                      color: Colors.blueAccent,
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: "Full Name",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(color: Colors.grey, width: 1),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(color: Colors.grey, width: 1),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: obseCure,
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1),
+                          ),
+                          suffixIcon: IconButton(
+                              icon: Icon(obseCure
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  obseCure = !obseCure;
+                                });
+                              })),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        cek();
+                      },
+                      child: CustomButton(
+                        "Sign UP",
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Container(
